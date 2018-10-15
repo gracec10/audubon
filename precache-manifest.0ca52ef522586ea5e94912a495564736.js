e60c6c826d8a6a63e2f6d1bbac452baead66ca2d
“GRACE <GRACE.CHEUNG@YALE.EDU>
2018-10-15 09:27:54 -0400

    Updates

---
+++ precache-manifest.0ca52ef522586ea5e94912a495564736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e53d65df56ae417a0d",
+    "url": "/audubon/static/js/runtime~main.51e53d65.js"
+  },
+  {
+    "revision": "6456cdffd3decc6dac4f",
+    "url": "/audubon/static/js/main.6456cdff.chunk.js"
+  },
+  {
+    "revision": "888493aad45b5e0b65aa",
+    "url": "/audubon/static/js/1.888493aa.chunk.js"
+  },
+  {
+    "revision": "6456cdffd3decc6dac4f",
+    "url": "/audubon/static/css/main.61a0d009.chunk.css"
+  },
+  {
+    "revision": "02a593f8e3dc040586edb3094dee813a",
+    "url": "/audubon/index.html"
+  }
+];
